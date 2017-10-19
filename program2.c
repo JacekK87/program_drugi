cd50b7f4070a59e3f5eeb1fd4f2c3f83d319e7c1
JacekK87 <jacekkielar1@wp.pl>
2017-10-19 17:51:46 +0200

    dwa kolejne programy

---
+++ program2.c
@@ -0,0 +1,19 @@
+
+/*dolacz biblioteke standardowa input-output*/
+#include <stdio.h>
+
+
+
+
+int main()
+{ 
+	
+	putchar('v'); /*wypis znak*/
+	putchar ('\n');
+	puts("wypisz string/ ciag znaków"); 
+
+	puts("\n Ala ma kota,\n a kot ma Ale");
+	return 0;
+
+
+}
